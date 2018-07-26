7ab1dc3f6b99bd81bb3ce4373606a16da22a5ffe
yuhao127 <yushaodi127@foxmail.com>
2018-07-26 22:47:06 +0800

    modified /rtl.f

--- sim/rtl.f
+++ sim/rtl.f
@@ -3,6 +3,7 @@
 
 ../ofdm_rtl/transmitter/data_scramble.v
 ../ofdm_rtl/transmitter/conv_code.v
+../ofdm_rtl/transmitter/long_training.v
 
 ../ofdm_rtl/receiver/
 ../ofdm_rtl/receiver/data_descramble.v
