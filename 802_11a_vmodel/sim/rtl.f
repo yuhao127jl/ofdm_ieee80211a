723b3b2a4b209a5ee16ac3f23df0a276be1035cd
yuhao127 <yushaodi127@foxmail.com>
2020-08-01 13:54:01 +0800

    1. add 802_11a_vmodel (hardware design with verilog).

---
+++ 802_11a_vmodel/sim/rtl.f
@@ -0,0 +1,15 @@
+
+../bench/top.v
+
+../ofdm_rtl/transmitter/data_scramble.v
+../ofdm_rtl/transmitter/conv_code.v
+../ofdm_rtl/transmitter/long_training.v
+
+../ofdm_rtl/receiver/
+../ofdm_rtl/receiver/data_descramble.v
+../ofdm_rtl/receiver/data_descramble.v
+
+
+
+
+
